9b068d6cbe2152eedc40758254cf3ab0da1d97ee
Heidi Ulery <heidi@compudharma.com>
2021-01-19 09:14:43 -0600

    Adding all setup files and gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+/* jshint node: true */
+'use strict';
+
+module.exports = function(grunt) {
+	grunt.initConfig({
+		jshint: {
+			client: [
+				'public/js/**/*.js',
+				'!public/js/vendor'
+			]
+		},
+		less: {
+			compile: {
+				files: {
+					'build/css/compiled.css': 'public/css/**/*.less'
+				}
+			}
+		},
+		concat: {
+			js: {
+				files: {
+					'build/js/bundle.js': 'public/js/**/*.js'
+				}
+			}
+		},
+		uglify: {
+			bundle: {
+				files: {
+					'build/js/bundle.min.js': 'build/js/bundle.js'
+				}
+			}
+		},
+		sprite: {
+			icons: {
+				src: 'public/img/icons/*.png',
+				dest: 'build/img/icons.png',
+				destCss: 'build/css/icons.css'
+			}
+		},
+		clean: {
+			js: 'build/js',
+			css: 'build/css',
+			img: 'build/img',
+			timestamp: 'build/timestamps.txt'
+		},
+		timestamp: {
+			options: {
+				file: 'build/timestamps.txt'
+			}
+		},
+		encrypt: {
+			encryptConfigFiles: {
+				options: {
+					key: '!NEWKIEbruno1',
+					dest: './',
+					ext: 'encrypted'
+				},
+				files: {
+					'configFiles': ['./secret.json']
+				}
+			},
+			decryptConfigFiles: {
+				options: {
+					key: '!NEWKIEbruno1',
+	 				dest: './',
+					decrypt: true
+				},
+				files: {
+					'configFiles': ['./secret.json.encrypted']
+				}
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-spritesmith');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-encrypt');
+	grunt.registerTask('default', ['jshint']); // register a default task alias
+	grunt.registerTask('js', 'Concatenate and minify static JS assets', 
+		['concat:js', 'uglify:bundle']);
+	grunt.registerTask('timestamp', function() {
+		let options = this.options({
+			file: '.timestamp'
+		});
+		let timestamp = +new Date();
+		let contents = timestamp.toString();
+
+		grunt.file.write(options.file, contents);
+	});
+};
